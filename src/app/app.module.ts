15e1ad00c36dd54e4ef47c240c92fa9c127e3cd5
Luis Molina <luis_molina@outlook.com>
2017-04-25 00:01:51 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MdToolbarModule, MdSidenavModule, MdMenuModule, MdButtonModule, MdIconModule, MdTabsModule } from "@angular/material";
+import "hammerjs";
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdMenuModule,
+    MdButtonModule,
+    MdIconModule,
+    MdTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
